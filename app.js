7ab028064e139f3a0f9b22ef98a60356cd001423
abiskar12345 <37185280+abiskar12345@users.noreply.github.com>
2020-09-13 11:39:24 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-
-const socketEvents = require('./api/socket/socket'); 
+// const socketEvents = require('./api/socket/socket'); 
 const partnerperferredRoutes = require("./api/routes/partnerperferred");
 const personaldetailsRoutes = require("./api/routes/personaldetails");
 const userRoutes = require('./api/routes/user');
+const blockRoutes= require('./api/routes/blockcontroller');
+const likeRoutes= require('./api/routes/likedprofile');
+const verificationRoutes = require('./api/auth/token_validation');
+const planRoutes= require('./api/routes/plan');
 
 mongoose.connect(
-  // "mongodb://mahajodiser:mahajodiser1@cluster0-shard-00-00.0n0bn.mongodb.net:27017,cluster0-shard-00-01.0n0bn.mongodb.net:27017,cluster0-shard-00-02.0n0bn.mongodb.net:27017/mahajodi?ssl=true&replicaSet=atlas-9pxmwp-shard-0&authSource=admin&retryWrites=true&w=majority" ,
 
   "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false",
   {
@@ -20,18 +21,6 @@ mongoose.connect(
   
 );
  mongoose.Promise = global.Promise;
-
-
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://mahajodiser:mahajodiser@cluster0.0n0bn.mongodb.net/mahajodi?retryWrites=true&w=majority";
-// const client = new MongoClient( uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   // const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   // client.close();
-// });
-
 
 app.use(morgan("dev"));
 app.use('/uploads', express.static('uploads'));
@@ -51,11 +40,18 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes which should handle requests
+
+
+//////////////////////////////////////////////////////// Routes which should handle requests////////////////////////////////////////////////////////////////////////
 app.use("/user/partnerperferred", partnerperferredRoutes);
 app.use("/user/personaldetails", personaldetailsRoutes);
 app.use("/user", userRoutes);
+app.use("/block", blockRoutes);
+app.use("/like", likeRoutes);
+app.use("/validate",verificationRoutes);
+app.use("/user/plan", planRoutes);
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
